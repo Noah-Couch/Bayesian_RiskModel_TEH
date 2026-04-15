0c9236e34818c7c4ed28b284c603449cd90b1793
Noah Couch <coucns24@cpu-amd-04.deac.wfu.edu>
2026-04-15 12:51:51 -0400

    Including saving plots in the sim loop

---
+++ Simulation_Study/Continuous_Outcomes/Helper_Functions/Fun-Plotting_Output.R
@@ -0,0 +1,88 @@
+
+Plotting_Output <- function(base_path, Outcome_SD) {
+  file_names <- list.files(base_path, pattern = paste0("\\OutcomeSD_",Outcome_SD,".csv$"), full.names = TRUE)
+  
+  list_csvs <- lapply(file_names, read_csv)
+  names(list_csvs) <- c(100, 200, 50)
+  
+  Power <- map_dfr(list_csvs, function(df) {
+    df |>
+      summarise(
+        Power.Risk = sum(rejection.Risk == "Reject", na.rm = TRUE) / N_iter,
+        Power.Risk_Noise = sum(rejection.Risk_Noise == "Reject", na.rm = TRUE) / N_iter,
+        Power.Intx = sum(rejection.interaction == "Reject", na.rm = TRUE) / N_iter,
+        Power.Intx_adj = sum(rejection.interaction_adj == "Reject", na.rm = TRUE) / N_iter,
+        Power.SDIR = sum(rejection.SDIR == "Reject", na.rm = TRUE) / N_iter
+      ) },
+    .id = "N") |>
+    mutate(N = as.numeric(N))
+  
+  Plot <- Power |>
+    pivot_longer(
+      cols = starts_with("Power"),
+      names_to = "Method",
+      values_to = "Power"
+    ) |>
+    mutate(Method = str_remove(Method, "Power.")) |>
+    mutate(
+      Method = case_when(Method == "Risk" ~ "Optimized Model",
+                         Method == "Risk_Noise" ~ "Non-optimized Model",
+                         Method == "Intx" ~ "Interaction",
+                         Method == "Intx_adj" ~ "Interaction (Adjusted)",
+                         TRUE ~ Method
+      )
+    ) |> 
+    mutate(label = if_else(N == max(N), Method, NA_character_)) |>
+    ggplot(aes(x = N, y = Power, group = Method)) + 
+    # Geometric annotations that play the role of grid lines
+    geom_vline(
+      xintercept = seq(50, 200, by = 25),
+      color = "grey91", 
+      size = .6
+    ) +
+    geom_segment(
+      data = tibble(y = seq(0, 1, by = 0.25), x1 = 40, x2 = 200),
+      aes(x = x1, xend = x2, y = y, yend = y),
+      inherit.aes = FALSE,
+      color = "grey91",
+      size = .6
+    ) +
+    geom_segment(
+      data = tibble(y = 0, x1 = 40, x2 = 200),
+      aes(x = x1, xend = x2, y = y, yend = y),
+      inherit.aes = FALSE,
+      color = "grey60",
+      size = .8
+    ) +
+    geom_point(aes(color = Method == "Optimized Model")) +
+    geom_line(aes(color = Method == "Optimized Model")) +
+    geom_text_repel(aes(color = Method == "Optimized Model", label = label),,
+                    family = "Lato",
+                    fontface = "bold",
+                    size = 2,
+                    direction = "y",
+                    xlim = c(220, NA),
+                    hjust = 0,
+                    segment.size = .7,
+                    segment.alpha = .5,
+                    segment.linetype = "dotted",
+                    box.padding = .4,
+                    segment.curvature = -0.1,
+                    segment.ncp = 3,
+                    segment.angle = 20,
+                    na.rm = TRUE) +
+    scale_x_continuous(
+      expand = c(0, 0),
+      limits = c(40, 275), 
+      breaks = seq(50, 200, by = 50)
+    ) +
+    scale_color_manual(values = c("TRUE" = "red", "FALSE" = "gray"))+
+    guides(color = "none") +
+    xlab("Sample Size, N") +
+    ylab("Power")
+  
+  
+  return(Plot)
+}
+
+
