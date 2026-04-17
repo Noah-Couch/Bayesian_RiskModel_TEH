9e67d71d7341b260a4e3caf716aa8852e5a3bf30
Noah Couch <coucns24@cpu-amd-11.deac.wfu.edu>
2026-04-17 12:39:31 -0400

    Final corrections to code before finalizing the summarized output and running the full simulation

--- Simulation_Study/Continuous_Outcomes/Helper_Functions/Fun-Plotting_Output.R
+++ Simulation_Study/Continuous_Outcomes/Helper_Functions/Fun-Plotting_Output.R
@@ -48,11 +48,14 @@ Plotting_Output <- function(base_path, Outcome_SD) {
   list_csvs <- lapply(file_names, read_csv)
   names(list_csvs) <- c(100, 200, 50)
   
+  t <- list_csvs[[1]]
+  
   Power <- map_dfr(list_csvs, function(df) {
     df |>
       summarise(
         Power.Risk = sum(rejection.Risk == "Reject", na.rm = TRUE) / N_iter,
         Power.Risk_Noise = sum(rejection.Risk_Noise == "Reject", na.rm = TRUE) / N_iter,
+        Power.MultInt = sum(rejection.Opt_MultInt == "Reject", na.rm = TRUE) / N_iter,
         Power.Intx = sum(rejection.interaction == "Reject", na.rm = TRUE) / N_iter,
         Power.Intx_adj = sum(rejection.interaction_adj == "Reject", na.rm = TRUE) / N_iter,
         Power.SDIR = sum(rejection.SDIR == "Reject", na.rm = TRUE) / N_iter
@@ -70,6 +73,7 @@ Plotting_Output <- function(base_path, Outcome_SD) {
     mutate(
       Method = case_when(Method == "Risk" ~ "Optimized Model",
                          Method == "Risk_Noise" ~ "Non-optimized Model",
+                         Method == "MultInt" ~ "Optimized Multiple Interactions",
                          Method == "Intx" ~ "Interaction",
                          Method == "Intx_adj" ~ "Interaction (Adjusted)",
                          TRUE ~ Method
@@ -116,8 +120,13 @@ Plotting_Output <- function(base_path, Outcome_SD) {
                     na.rm = TRUE) +
     scale_x_continuous(
       expand = c(0, 0),
-      limits = c(40, 275), 
+      limits = c(40, 300), 
       breaks = seq(50, 200, by = 50)
+    ) +
+    scale_y_continuous(
+      expand = c(0, 0),
+      limits = c(0, 1), 
+      breaks = seq(0, 1, by = 0.25)
     ) +
     scale_color_manual(values = c("TRUE" = "red", "FALSE" = "#48494B"))+
     guides(color = "none") +
